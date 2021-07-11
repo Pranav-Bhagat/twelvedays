4b058f167919f62b916969150a5c794b8e1ba7cd
Pranav-Bhagat <81111220+Pranav-Bhagat@users.noreply.github.com>
2021-07-11 15:42:50 -0400

    Update pluralize_gift.R

--- R/pluralize_gift.R
+++ R/pluralize_gift.R
@@ -1,20 +1,16 @@
-#' Takes a noun and makes it plural
-#'
-#' @param gift A string or vector of strings
-#'
-#' @return A string or vector of strings with the pluralized words
-#'
-#' @import stringr
-#' @import dplyr
-#' @import glue
-#' @import purrr
-#'
-#' @export
-pluralize_gift <- function(gift){
-
-gift <- gift %>%
-  str_replace()
-
-return(gift)
-
+pluralize_gift <- function(gift)
+{
+  if (str_detect(gift, "y")) 
+  {
+    gift <- str_replace(gift, "y", "ies")
+  }
+  else if (str_detect(gift, "oo")) 
+  {
+    gift <- str_replace(gift, "oo", "ee")
+  }
+  else 
+  {
+    gift <- paste0(gift, "s")
+  }
+  return(gift)
 }
