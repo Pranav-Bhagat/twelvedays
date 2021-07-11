d6cabefb613c32e86d20cadc023f235efa199277
Pranav-Bhagat <81111220+Pranav-Bhagat@users.noreply.github.com>
2021-07-11 15:43:18 -0400

    Update make_phrase.R

--- R/make_phrase.R
+++ R/make_phrase.R
@@ -1,29 +1,29 @@
-#' Puts the various parts of speech together into a full phrase.
-#'
-#' @param num An integer
-#' @param num_word A string corresponding to the integer
-#' @param item A string
-#' @param verb A string
-#' @param adjective A string
-#' @param location A string
-#'
-#' @return A string containing the words in grammatical order.
-#'
-#' @import stringr
-#' @import glue
-#' @import dplyr
-#' @import purrr
-#'
-#' @export
-
-
-
-make_phrase <- function(num, num_word, item, verb, adjective, location){
-
-  verb <- str_replace_na(verb, "")
-
-  #????
-
-
+make_phrase <- function(num, num_word, item, verb, adjective, location) 
+{
+  item <- replace_na(item, "")
+  verb <- replace_na(verb, "")
+  adjective <- replace_na(adjective, "")
+  location <- replace_na(location, "")
+  
+  vowels = c("a", "e", "i", "o", "u")
+  
+  if (num > 1) 
+  {
+    item <- pluralize_gift(item)
+    begword <- num_word
+  } else 
+  {
+    if(sum(substring(item, 1, 1) == vowels) == 1) 
+    {
+      begword <- "an"
+    } 
+    else 
+    {
+      begword <- "a"
+    }
+  }
+  
+  ans <- paste(begword, adjective, item, verb, location, sep = " ")
+  ans <- gsub("\\s+", " ", ans)
+  return(ans)
 }
-
