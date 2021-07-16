eee0f70a5fad6f1320b399f7fcdcec8aacd25a29
Pranav-Bhagat <81111220+Pranav-Bhagat@users.noreply.github.com>
2021-07-16 12:17:38 -0400

    Delete Lab7ChallengePart2SampleMeanNS.R

--- R/Lab7ChallengePart2SampleMeanNS.R
+++
@@ -1,6 +0,0 @@
-sample_means_ns <- function(vec, reps, ns) {
-  df <- map_dfc(ns, ~many_sample_means(vec, reps, .x))
-  colnames(df) <- ns
-  return(df %>%
-           pivot_longer(colnames(df), names_to = "n", values_to = "sample_mean") %>%
-           mutate(n = as.double(n)))}
